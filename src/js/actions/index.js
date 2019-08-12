275cf2a368ad83a8a8edd527af2d0aa7f5a0524e
ibenge <benjamin.uforo@gmail.com>
2019-08-12 10:14:43 +0100

    first redux commit

---
+++ src/js/actions/index.js
@@ -0,0 +1,5 @@
+import { ADD_ARTICLE } from '../constants/index';
+
+export function addArticle(payload) {
+    return { type:ADD_ARTICLE, payload }
+  };
---
+++ src/js/store/index.js
@@ -0,0 +1,9 @@
+import {createStore } from 'redux'
+
+import rootReducer from '../reducers/index'
+
+const store = createStore(rootReducer)
+
+store.subscribe(()=>console.log(store.getState()));
+
+export default store;
